b726e4fb3fb10c358b4a5056493c22741c5da224
Ruslan Smirnov <smirnov_ruslan@outlook.com>
2022-10-07 11:41:30 +0300

    Fix gen datalist script

--- quality/generate_pseudo_labels/gen_datalist.py
+++ quality/generate_pseudo_labels/gen_datalist.py
@@ -1,34 +1,64 @@
-from torch.utils.data import DataLoader
+import argparse
+import os
+from pathlib import Path
+
 from torchvision.datasets import ImageFolder
 from tqdm import tqdm
 
 
-def gentxt(data_root, outfile):         # generate data file via traveling the target dataset 
-    '''
-    Use ImageFolder method to travel the target dataset 
-    Save to two files including ".label" and ".labelpath"
-    '''
+class GenDatalistNamespace(argparse.Namespace):
+    outfile: str = './DATA.label'
+    data_root: str
+
+
+def parse_args() -> GenDatalistNamespace:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--outfile', help='File to write results')
+    parser.add_argument('data_root', help='Path to dataset root')
+
+    return parser.parse_args(namespace=GenDatalistNamespace())
+
+
+def gentxt(data_root: str, outfile: str) -> int:
+    """
+    Use ImageFolder method to travel the target dataset
+    Save to two files including '.label' and '.labelpath'
+    """
+
     # output file1
-    outfile1 = open(outfile, 'w')
+    outfile_1 = open(outfile, 'w')
     # output file2
-    outfile2 = open(outfile+'path', 'w') 
+    outfile_2 = open(outfile + 'path', 'w')
     data = ImageFolder(data_root)
-    count = 0
-    tqdm(data.imgs)
+    image_counter = 0
+
     # travel the target dataset
-    for index, value in enumerate(data.imgs):
-        count += 1
-        img = '/'+'/'.join(value[0].split('/')[-2:])
-        outfile1.write(img + '\n')
-        outfile2.write(value[0] + '\t' + str(value[1]) + '\n')
-    return count
+    for (image_path, person_index) in tqdm(data.imgs, total=len(data)):
+        image_counter += 1
+
+        path = Path(image_path)
+        img = os.path.join(*path.parts[-2:])
+
+        print(img, file=outfile_1)
+        print(str(path) + '\t' + str(person_index), file=outfile_2)
+
+    outfile_1.close()
+    outfile_2.close()
+
+    return image_counter
 
 
-if __name__ == "__main__":                  # obtain data list
-    '''
+def main(args: GenDatalistNamespace):
+    gentxt_res = gentxt(args.data_root, args.outfile)
+    print(f'Total images: {gentxt_res}')
+
+
+if __name__ == "__main__":
+    """
     This method is to obtain data list from dataset
     and save to txt files
-    '''
-    outfile = './DATA.label'
-    data_root = '/data/home/fuzhaoou/Dataset/MS1M/MS1M_arcface_112'
-    print(gentxt(data_root, outfile))
+    """
+
+    args = parse_args()
+    main(args)
